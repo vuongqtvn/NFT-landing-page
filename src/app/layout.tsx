95bef787a9d5630a099cab5823532ff2d2db8ffb
huutin2502 <tindh@danateq.vn>
2025-07-29 02:23:03 +0700

    fix build error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,18 +2,16 @@ import type { Metadata } from 'next'
 import { Barlow_Condensed, Fira_Code } from 'next/font/google'
 import './globals.css'
 
-export const barlowCondensedSans = Barlow_Condensed({
+const barlowCondensedSans = Barlow_Condensed({
   variable: '--font-barlow-sans',
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700', '800', '900'],
-  display: 'swap',
 })
 
-export const firaCodeMono = Fira_Code({
+const firaCodeMono = Fira_Code({
   subsets: ['latin'],
   weight: ['300', '400', '500', '600', '700'],
   variable: '--font-fira-mono',
-  display: 'swap',
 })
 
 export const metadata: Metadata = {
